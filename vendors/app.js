69bec904473995c739ecb3d26a787baec6db564b
Elena Zlateva <elena.zlateva84@gmail.com>
2017-11-06 22:56:21 +0200

    Added the project files

---
+++ vendors/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('.toggle-nav').click(function(e) {
+        $(this).toggleClass('active');
+        $('nav.menu ul').slideToggle();
+        e.preventDefault();
+    });
+
+    $("button.btn").click(function() {
+        $(".select-lang ul").slideToggle();
+    })
+
+    $('.accordion-section-title').click(function(e) {
+
+        if ($(e.target).is('.active')) {
+            $('.accordion .accordion-section-title').removeClass('active');
+            $('.accordion .accordion-section-content.open').slideUp(300).removeClass('open');
+        } else {
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $(this).next().slideDown(300).addClass('open');
+        }
+    });
+
+
+
+    $.scrollify({
+        section: "section",
+        sectionName: false,
+        interstitialSection: "header,footer",
+        before: function(i, sections) {
+            if (i < 2) {
+                $("header").css({ "background": "rgba(0,0,0,0.55)" });
+            } else {
+                $("header").css({ "background": "#333" });
+                $.scrollify.setOptions({ offset: -126 })
+            }
+        }
+    });
+});
